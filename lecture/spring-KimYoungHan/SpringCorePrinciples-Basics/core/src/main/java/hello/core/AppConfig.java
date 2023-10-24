813ee86e92ff36f923758a0987f46c50afbca1e3
LimDae94 <piay801@naver.com>
2023-10-24 20:30:41 +0900

    섹션5_ 프록시 컨테이너 직전
    섹션5_ 프록시 컨테이너 직전까지 @Configuration  사용 유무에 다른 의존 여

--- lecture/spring-KimYoungHan/SpringCorePrinciples-Basics/core/src/main/java/hello/core/AppConfig.java
+++ lecture/spring-KimYoungHan/SpringCorePrinciples-Basics/core/src/main/java/hello/core/AppConfig.java
@@ -3,11 +3,13 @@ package hello.core;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,17 +17,20 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public MemoryMemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
-    @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
+    public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
