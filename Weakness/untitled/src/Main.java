67969f45b6b2f4d1b8fa001239d043571ba76913
LimDae94 <piay801@naver.com>
2023-11-06 11:30:52 +0900

    enum 실습 예제 코드
    enum 실습 예제 코드

--- Weakness/untitled/src/Main.java
+++ Weakness/untitled/src/Main.java
@@ -1,14 +1,39 @@
-import java.util.TreeSet;
-
 public class Main {
-    public static void main(String[] args) {
-        TreeSet set = new TreeSet();
-        int[] score = {80, 95, 50, 35, 45, 65, 10, 100};
+    // 계절을 나타내는 열거형 정의
+    enum Season {
+        SPRING, SUMMER, FALL, WINTER
+    }
 
-        for (int i = 0; i < score.length; i++) {
-            set.add(new Integer(score[i]));
+    public static void main(String[] args) {
+        // 열거형 상수 사용
+        Season currentSeason = Season.SUMMER;
+        System.out.println("Current season is: " + currentSeason);
+
+        // 열거형 상수 비교
+        if (currentSeason == Season.SUMMER) {
+            System.out.println("It's a great time for outdoor activities!");
         }
-        System.out.println("50보다 작은 값: " + set.headSet(new Integer(50)));
-        System.out.println("50보다 큰 값: " + set.tailSet(new Integer(50)));
+
+        // switch 문으로 열거형 사용
+        switch (currentSeason) {
+            case SPRING:
+                System.out.println("Spring is the season of renewal.");
+                break;
+            case SUMMER:
+                System.out.println("Summer is all about fun in the sun.");
+                break;
+            case FALL:
+                System.out.println("Fall brings colorful leaves and cooler weather.");
+                break;
+            case WINTER:
+                System.out.println("Winter is the time for snow and holiday festivities.");
+                break;
+        }
+
+        // 열거형의 모든 상수 순회
+        System.out.println("All seasons:");
+        for (Season season : Season.values()) {
+            System.out.println(season);
+        }
     }
 }
