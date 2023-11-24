4ed4862304147b69e0841a2a909116efed9bad2f
LimDae94 <piay801@naver.com>
2023-11-24 15:31:38 +0900

    초기 설정
    초기 설정

---
+++ books/BeingASpringBoot3BackendDeveloperJava/spblog/src/main/java/limdae/spblog/SpblogApplication.java
@@ -0,0 +1,13 @@
+package limdae.spblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpblogApplication.class, args);
+	}
+
+}
---
+++ books/BeingASpringBoot3BackendDeveloperJava/spblog/src/test/java/limdae/spblog/SpblogApplicationTests.java
@@ -0,0 +1,13 @@
+package limdae.spblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
