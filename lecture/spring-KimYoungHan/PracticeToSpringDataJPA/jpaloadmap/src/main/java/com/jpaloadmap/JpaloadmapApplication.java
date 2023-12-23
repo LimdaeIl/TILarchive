0865d1b246b514ab8b7de148c71619399645bb2b
LimDae94 <piay801@naver.com>
2023-12-23 21:59:06 +0900

    정리
    정리

---
+++ lecture/spring-KimYoungHan/PracticeToSpringDataJPA/jpaloadmap/src/main/java/com/jpaloadmap/JpaloadmapApplication.java
@@ -0,0 +1,13 @@
+package com.jpaloadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaloadmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaloadmapApplication.class, args);
+	}
+
+}
