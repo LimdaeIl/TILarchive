174d53b87219322dc1fcee7d0fd31ce1cdfd7c02
LimDae94 <piay801@naver.com>
2023-12-28 16:45:55 +0900

    업데이트
    업데이트

--- books/BeingASpringBoot3BackendDeveloperJava/example/src/main/java/security/example/service/UserDetailService.java
+++ books/BeingASpringBoot3BackendDeveloperJava/example/src/main/java/security/example/service/UserDetailService.java
@@ -14,7 +14,7 @@ public class UserDetailService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    // 사용자 이름(email)으로 사용자의 정보를 가져오는 메서드
+    // 사용자 이름(email)으로 사용자의 정보를 가져오는 메서드!
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException(email));
