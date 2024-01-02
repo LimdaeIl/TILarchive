423960c5e3ae8dfc77c26ee77daa214b63e50974
LimDae94 <piay801@naver.com>
2024-01-02 14:37:47 +0900

    restful 설계 학습하기
    실습 내용 추가

---
+++ lecture/spring-DowonLee/my-restful-service/src/main/java/kr/co/joneconsulting/myrestfulservice/MyRestfulServiceApplication.java
@@ -0,0 +1,20 @@
+package kr.co.joneconsulting.myrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MyRestfulServiceApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ac = SpringApplication.run(MyRestfulServiceApplication.class, args);
+
+        String[] allBeanNames = ac.getBeanDefinitionNames();
+        for (String allBeanName : allBeanNames) {
+            System.out.println("allBeanName = " + allBeanName);
+        }
+    }
+
+}
