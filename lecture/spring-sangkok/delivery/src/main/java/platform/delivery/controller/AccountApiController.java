eba37f988305afea8060f99b930d85ee85ec9b83
LimDae94 <piay801@naver.com>
2024-01-08 18:25:25 +0900

    컨트롤러 생성
    컨트롤러 생성

---
+++ lecture/spring-sangkok/delivery/src/main/java/platform/delivery/controller/AccountApiController.java
@@ -0,0 +1,17 @@
+package platform.delivery.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/account")
+public class AccountApiController {
+
+    @PostMapping("/login")
+    public void login(
+
+    ) {
+
+    }
+}
---
+++ lecture/spring-sangkok/delivery/src/main/java/platform/delivery/model/LoginRequest.java
@@ -0,0 +1,14 @@
+package platform.delivery.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming
+public class LoginRequest {
+}
