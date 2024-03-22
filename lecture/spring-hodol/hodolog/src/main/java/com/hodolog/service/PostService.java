a38aed742ca9283b52005c220d664647a73344cd
LimDae94 <piay801@naver.com>
2024-03-22 18:44:53 +0900

    빌더 패턴 수정

--- lecture/spring-hodol/hodolog/src/main/java/com/hodolog/service/PostService.java
+++ lecture/spring-hodol/hodolog/src/main/java/com/hodolog/service/PostService.java
@@ -56,20 +56,28 @@ public class PostService {
 
         PostEditor.PostEditorBuilder editorBuilder = post.toEditor();
 
-        // 검증하면서 데이터를 넣기
-//        if (postEdit.getTitle() != null) {
-//            editorBuilder.title(postEdit.getTitle());
-//        }
-//
-//        if (postEdit.getContent() != null) {
-//            editorBuilder.content(postEdit.getContent());
-//        }
-
         PostEditor postEditor = editorBuilder.title(postEdit.getTitle())
                 .content(postEdit.getContent())
                 .build();
 
         post.edit(postEditor);
 
+        // PostEditor 로 작성한 이유는 edit 필드가 많아지면 아래처럼 처리하기 힘듭니다.
+        // Post 가 스스로 수정할 수 있는 포인트는 타이틀과 제목 같은 제한 포인트를 두기 위해 PostEditor 를 사용합니다.
+        // 프론트 개발자 입장에서 생각해보면, 제목: 호돌맨 내용: 반포자이-> 초가집 으로 수정한다고 가정합니다.
+        // 타이틀은 수정안하니까 null 로 보내고, 컨텐트는 초가집으로 보내게 되는 경우가 있습니다.
+        // 아래처럼하면 검증을 안하고 그냥 postEdit.getTitle() 을 때려 넣은 것이기 때문에 문제가 됩니다.
+        // postEdit.getTitle() != null ? postEdit.getTitle() : post.getTitle(); 으로 처리해야 합니다.
+        // 기존의 저장된 제목을 유지하거나 수정된 제목을 넣어야 한다는 것입니다.
+        // 이러한 문제점은 코드 가독성이나 유지 보수가 어렵기 때문에 PostEditor 을 사용하는 것입니다.
+//        post.edit(postEdit.getTitle(), postEdit.getContent());
+
     }
+
+//    public void delete(Long id {
+//        Post post = postRepository.findById(id)
+//                .orElseThrow(PostNotFound::new);
+//
+//        postRepository.delete(post);
+//    })
 }
